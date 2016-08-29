b2c7f86a2e85430d83325637c700f4a965df27d0
Pedro de Oliveira <pedrooliveira2309@gmail.com>
2016-08-29 14:01:23 -0300

    Definindo pasta com queries e criando fução que retorna motd (incompleto)

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var server = require('net');
 
 var net = server.createServer(function(socket) {
-	socket.write('Echo server\r\n');
+	console.log(motdQ);
+	socket.write('');
 	socket.pipe(socket);
 });
+
+var motdQ = function motdQuery() {
+	var fs = require('fs');
+ 
+	fs.readFile('queries/motd', 'utf8', function(err, contents) {
+		return contents;
+	});
+	}
 
 net.listen(6667, '127.0.0.1');

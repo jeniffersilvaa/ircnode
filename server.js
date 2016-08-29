fd34012dcd52026d6727ad8954dfa8f20098ebdb
Pedro de Oliveira <pedrooliveira2309@gmail.com>
2016-08-29 13:18:13 -0300

    Definindo sockets de conexão lado servidor

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-var server = require(net);
+var server = require('net');
+
+var net = server.createServer(function(socket) {
+	socket.write('Echo server\r\n');
+	socket.pipe(socket);
+});
+
+net.listen(6667, '127.0.0.1');

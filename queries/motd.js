c4d97363957a6fb66990783763ebf8a94577a912
Pedro de Oliveira <pedrooliveira2309@gmail.com>
2016-08-30 13:51:55 -0300

    Correção na instanciação da variavel texto

--- queries/motd.js
+++ queries/motd.js
@@ -1,7 +1,6 @@
 exports.print = function () {
 		var fs = require('fs');
- 		var texto = "alguma coisa";
-		texto = fs.readFileSync('./queries/config/motd', {encoding: 'utf8'}, function(erro, dados) {
+		var texto = fs.readFileSync('./queries/config/motd', {encoding: 'utf8'}, function(erro, dados) {
 			if(erro) {
 				console.log("Ocorreu um erro na leitura do arquivo './queries/config/motd'!");	
 			}
